871a4227e7f35d6cafdd38aacdd9503812dc7090
Mahendra Chari <mahi.chari2010@gmail.com>
2023-06-12 00:29:40 +0530

    added

--- js/main.js
+++ js/main.js
@@ -2,20 +2,60 @@ var gameOver=false;
 const makeBoard = () => {
   let el = document.getElementById("board");
   let num;
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 10; i >= 1; i--) {
     let row = document.createElement("div");
     row.setAttribute("id", "row" + i);
     for (j = 1; j <= 10; j++) {
       num = 10 * (i - 1) + j;
       let col = document.createElement("span");
-      col.setAttribute("id", "item" + j);
+      col.setAttribute("id", "item"+num);
       let textN = document.createTextNode(num);
       col.appendChild(textN);
       row.appendChild(col);
     }
     el.appendChild(row);
+
   }
 };
+const updateTheBoard = (playerNum) => {
+    //let el = document.getElementById("board");
+    let num;
+    //let playerScore=Object.keys(sessionStorage).filter((item)=>item.includes('playerScore'));
+    let playerScore=sessionStorage.getItem('playerScore'+playerNum);
+    //for(j=0;j<playerScore.length;j++){
+        //console.log("item"+playerScore[j+1])
+    //remvoe the element if it's there
+    let oldEl=document.getElementById('player-position'+playerNum);
+    if(oldEl){
+        oldEl.remove();
+    }
+    let el=document.getElementById("item"+playerScore);
+
+    if(el){
+        //el.classList.add('activeGrid');
+        let playerPosEl=document.createElement("span");
+        playerPosEl.setAttribute('id','player-position'+playerNum);
+        playerPosEl.style.color='red'
+        playerPosEl.textContent=playerNum;
+        el.appendChild(playerPosEl)
+    }
+    //}
+    //console.log(Players);
+    /*for (let i = 10; i >= 1; i--) {
+      let row = document.createElement("div");
+      row.setAttribute("id", "row" + i);
+      for (j = 10; j >= 1; j--) {
+        num = 10 * (i - 1) + j;
+        let col = document.createElement("span");
+        col.setAttribute("id", "item" + j);
+        let textN = document.createTextNode(num);
+        col.appendChild(textN);
+        row.appendChild(col);
+      }
+      el.appendChild(row);
+  
+    }*/
+  };
 
 const showDiceVal=(diceValEl,randVal)=>{
     diceValEl.innerHTML=''
@@ -33,13 +73,15 @@ const rollDice=(playerNum)=>{
         randVal=Math.round(Math.random()*6);
     }
 
-    pos=sessionStorage.getItem('player'+playerNum) || 0;
-    pos=parseInt(pos)+randVal;
-    sessionStorage.setItem('player'+playerNum,pos)
-    
     turn=sessionStorage.getItem('player-'+playerNum+'turns') || 0;
     turn=parseInt(turn)+1;
+    if(turn )
     sessionStorage.setItem('player-'+playerNum+'turns',turn)
+
+    pos=sessionStorage.getItem('playerScore'+playerNum) || 0;
+    pos=parseInt(pos)+randVal;
+    sessionStorage.setItem('playerScore'+playerNum,pos)
+       
 
     let diceValEl=document.getElementById("dice-output");
     diceValEl.innerHTML=''
@@ -52,29 +94,28 @@ const rollDice=(playerNum)=>{
        }
     ,200);
     let turnEl=document.getElementsByClassName('player-col-turn');
-    for(let i=0;i<turnEl.length;i++){
-        turnEl[i].innerHTML=sessionStorage.getItem('player-'+(i+1)+'turns')
-    }
+    turnEl[playerNum-1].innerHTML=sessionStorage.getItem('player-'+playerNum+'turns')
 
     let resEl=document.getElementsByClassName('player-col-res');
-    for(let i=0;i<resEl.length;i++){
-        resEl[i].innerHTML=sessionStorage.getItem('player'+(i+1))
-    }
-    
+    resEl[playerNum-1].innerHTML=sessionStorage.getItem('playerScore'+playerNum)
+    //move the players position
+    document.getElementsByClassName('player-col-res');
     
     //check if the game is over
     for(let j=1;j<=sessionStorage.getItem('players');j++){
-        if(sessionStorage.getItem('player'+j) > 100){
+        if(sessionStorage.getItem('playerScore'+j) > 100){
             alert('Game Over!! winner is player '+j);
             document.getElementById('player-row-'+j).style.background='green'
             
         }
     }
-    store=sessionStorage.
-    console.log(randVal)
+    updateTheBoard(playerNum);
 }
 const createPlayers = () =>{
     let playersEl=document.getElementById("players");
+    while(playersEl.firstChild){
+        playersEl.removeChild(playersEl.firstChild)
+    }
     let players=sessionStorage.getItem('players');
     try{
         while(playersEl.firstChild){
@@ -130,7 +171,10 @@ const selectNoOfPlayers=()=>{
     while(totalPlayers > 3){
             totalPlayers=window.prompt('# of players at max is 3, please select accordingly');
     }
-    
+    allKeys=Object.keys(sessionStorage)
+    allKeys.forEach(
+        (key) => sessionStorage.removeItem(key)
+    )
     if(sessionStorage){
         sessionStorage.clear;
         sessionStorage.setItem('players',totalPlayers)
@@ -153,9 +197,15 @@ const play=()=>{
     }
 }
 const startGame=()=>{
+    selectNoOfPlayers();
+    //clear the data
+
     document.getElementById('players').style.display='block'
     let ele=document.querySelectorAll("[id^='roll-']");
     console.log('Starting the Game')
+    //scroll to board
+    document.getElementById('board').scrollIntoView();
+
     for (let i = 0; i <= ele.length; i++) {
         ele[i].addEventListener("click", function() {
             rollDice(i+1);
